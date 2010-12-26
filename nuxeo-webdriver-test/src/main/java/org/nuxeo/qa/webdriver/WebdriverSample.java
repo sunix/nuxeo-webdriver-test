24d46fae41ce1f63cd2fe790c5eb58401feb23ac
Sun Seng David TAN <stan@nuxeo.com>
2010-12-26 14:51:00 +0100

    ajax doesn't work

--- nuxeo-webdriver-test/src/main/java/org/nuxeo/qa/webdriver/WebdriverSample.java
+++ nuxeo-webdriver-test/src/main/java/org/nuxeo/qa/webdriver/WebdriverSample.java
@@ -2,7 +2,9 @@ package org.nuxeo.qa.webdriver;
 
 import java.net.URL;
 import org.nuxeo.qa.webdriver.driver.users.LoginDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class WebdriverSample {
@@ -12,6 +14,18 @@ public class WebdriverSample {
         LoginDriver logindriver = new LoginDriver(driver, new URL(
                 "http://localhost:8080/nuxeo/"));
         logindriver.login("Administrator", "Administrator", "en_US");
+
+        driver.findElement(By.linkText("Bree Van de Kaamp")).click();
+        driver.findElement(By.linkText("Test incoming pia document")).click();
+        driver.findElement(By.linkText("Distribute")).click();
+
+        WebElement nxw_reponse_recipients_suggest = driver.findElement(By.id("distribution_participants:nxl_cm_participants:nxw_reponse_recipients_suggest"));
+        nxw_reponse_recipients_suggest.sendKeys("Bre");
+
+        WebElement ajaxUserListElement = driver.findElement(By.xpath("//table[@id='distribution_participants:nxl_cm_participants:nxw_reponse_recipients_suggestionBox:suggest']/tbody/tr[1]/td[2]"));
+
+        String value = ajaxUserListElement.getText();
+        System.out.println(value);
 
         driver.quit();
     }
